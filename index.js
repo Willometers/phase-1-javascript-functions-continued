c3561582b4e2084cbafab3f580fc4c7db4c239e5
BillyMiles <williammiles201@gmail.com>
2021-04-27 10:44:25 -0400

    commit tree 4a423aa0dccc87a0b2d06c54093522844a99d226

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function saturdayFun(activity=`roller-skate`) {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity=`go to the office`) {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair="*") {
+        return function(part1="a dedicated programmer") {
+            return `You are ${flair}${part1}${flair}!`
+        }
+    }
+
+// function demoChain(name) {
+//     const part1 = 'hi'
+//     return function() {
+//       const part2 = 'there'
+//       return function() { 
+//         console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+//       }
+//     }
+//   }
+//   demoChain("Dr. Stephen Strange")()() //=> HI there Dr. Stephen Strange 
